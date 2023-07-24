938488f644dc847875d51e9c73eb1b3f579fce06
Alexa VanSchaardenburg <alexa.vsdb@gmail.com>
2023-07-24 10:47:37 -0600

    Imported and mapped over the card component to add fetched tricks to dom

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,29 @@
 import './App.css';
-import React from 'react';
+import { React, useEffect, useState } from 'react';
+import Card from '../Card/Card.js'
+import Form from '../Form/Form.js'
 
+const App = () => {
 
+  const [allTricks, setAllTricks] = useState([])
 
-function App() {
+  useEffect(() => {
+    fetch('http://localhost:3001/api/v1/tricks')
+      .then(res => res.json())
+      .then(res => setAllTricks(res))
+  }, [])
+
+  const addTrick = (newTrick) => {
+    setAllTricks([...allTricks, newTrick])
+  }
+
   return (
     <div className="App">
       <h1>Sick Trick Wish List</h1>
+      <Form addTrick={addTrick}/>
+      <div className='cards'>
+        {allTricks.map(thisTrick => <Card thisTrick={thisTrick}/>)}
+      </div>
     </div>
   );
 }
